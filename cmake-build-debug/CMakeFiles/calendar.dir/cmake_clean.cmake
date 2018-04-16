00c49f50821edddd50e266bb46c5f63c8c374b3c
Luna Alison Alvarez <lunaalisonalvarez@gmail.com>
2018-04-15 17:57:49 -0700

    oops again

---
+++ cmake-build-debug/CMakeFiles/calendar.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calendar.dir/main.cpp.o"
+  "calendar.pdb"
+  "calendar"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calendar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
