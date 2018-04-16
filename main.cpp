3a4f342603ff0addc08c1ac400ef021988422163
Luna Alison Alvarez <lunaalisonalvarez@gmail.com>
2018-04-15 17:42:41 -0700

    init

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+/*
+ * write a program that prompts the user to enter the year and first day of the year
+ * and displays the calendar table for the year on the console.
+ *
+ *  */
+
+int main() {
+
+}
